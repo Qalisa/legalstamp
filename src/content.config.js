ee07efa90a7fbce2196d752e32ed3f1138922e2d
Guillaume VARA <guillaume.vara@gmail.com>
2025-02-25 11:32:21 +0100

    wip

---
+++ src/content.config.js
@@ -0,0 +1,15 @@
+// https://docs.astro.build/en/guides/content-collections/
+
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const legalstamped = defineCollection({ 
+    loader: glob({ pattern: "**/*.md", base: "./legalstamped/generated" }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { legalstamped };
